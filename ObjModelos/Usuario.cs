4e9f5e9330e75168048ac0bcb89af7482c5e10db
RobertRuas <ro.ru@aol.com>
2015-10-08 00:07:56 -0300

    Classe de Modelos pronta

---
+++ ObjModelos/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ObjModelos
+{
+    class Usuario
+    {
+        public int IDUsuario { get; set; }
+
+        public string Usuario { get; set; }
+
+        public int IDSexo { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Email { get; set; }
+
+        public string UsuarioNome { get; set; }
+
+        public string UsuarioSenha { get; set; }
+
+        public int IDPrevilegios { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public int IDStatus { get; set; }
+    }
+}
